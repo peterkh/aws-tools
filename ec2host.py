67a245b46da42d7694e9d73df90941a9072e5f8a
Peter Hall <peter.hall@catchoftheday.com.au>
2014-02-27 15:04:54 +1100

    fixing bug: works if you don't have a AWS_DEFAULT_REGION set in env added a "script_mode" with -s which will just print a list of private IPs. Good for piping to csshx

--- ec2host.py
+++ ec2host.py
@@ -5,29 +5,27 @@ import ConfigParser
 import os
 from os.path import expanduser
 
-
+default_region_env = os.environ.get('AWS_DEFAULT_REGION')
 
 config = ConfigParser.SafeConfigParser()
-config.set('DEFAULT', 'region', os.environ.get('AWS_DEFAULT_REGION'))
+if default_region_env:
+    config.set('DEFAULT', 'region', default_region_env)
+else:
+    config.set('DEFAULT', 'region', 'ap-southeast-2')
 config.set('DEFAULT', 'role_tag', 'role')
 config.read("%s/.awstools.conf" % (expanduser("~")))
 
 conf_parser = argparse.ArgumentParser()
-conf_parser.add_argument("-r", "--region", dest="region", help="EC2 region instances are located in")
-conf_parser.add_argument("-t", "--tag", dest="tag", help="The AWS tag name to search for instances with")
+conf_parser.add_argument("-r", "--region", dest="region", default=config.get('DEFAULT', 'region'), help="EC2 region instances are located in")
+conf_parser.add_argument("-t", "--tag", dest="tag", default=config.get('DEFAULT', 'role_tag'), help="The AWS tag name to search for instances with")
+conf_parser.add_argument("-s", "--script_mode", action='store_true', default=False, help="Output in script friendly mode (IP address list only)")
 conf_parser.add_argument("role_name")
 
 args = conf_parser.parse_args()
 
-if args.region:
-    region = args.region
-else:
-    region = config.get('DEFAULT', 'region')
-if args.tag:
-    tag = args.tag
-else:
-    tag = config.get('DEFAULT', 'role_tag')
-
+region = args.region
+tag = args.tag
+script_mode = args.script_mode
 filters = []
 
 if tag.find(',') >= 0:
@@ -36,8 +34,9 @@ if tag.find(',') >= 0:
 else:
     filters.append({ "tag:%s" % (tag): args.role_name })
 
-print "Region: %s" % (region)
-print "Hosts with tag: %s:%s" % (tag, args.role_name)
+if not script_mode:
+    print "Region: %s" % (region)
+    print "Hosts with tag: %s:%s" % (tag, args.role_name)
 
 ec2_conn = ec2.connect_to_region(region)
 
@@ -47,5 +46,8 @@ for ec2_filter in filters:
     instances = [i for r in reservations for i in r.instances]
 
     for instance in instances:
-        print "%s %s %s %s" % (instance.tags['Name'], instance.id, instance.private_ip_address, instance.ip_address)
+        if script_mode:
+            print instance.private_ip_address
+        else:    
+            print "%s %s %s %s" % (instance.tags['Name'], instance.id, instance.private_ip_address, instance.ip_address)
 
