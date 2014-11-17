c81602add590dcdffaaedb36778a7c26c944fe32
Peter Hall <phall@odecee.com.au>
2014-11-17 14:44:05 +1100

    Adding support for boto profiles and filtering role tags for instance tags only

--- ec2host.py
+++ ec2host.py
@@ -7,25 +7,26 @@ import shlex
 from os.path import expanduser
 from subprocess import check_call
 
-def get_instances(region, tags, tag_value):
+def get_instances(region, tags, tag_value, profile):
     filters = []
     all_instances = []
     for tag in tags:
         filters.append({ "tag:%s" % (tag): tag_value })
 
+    print "using profile: %s" % profile
     for ec2_filter in filters:
-        ec2_conn = ec2.connect_to_region(region)
+        ec2_conn = ec2.connect_to_region(region, profile_name=profile)
         reservations = ec2_conn.get_all_instances(filters=ec2_filter)
         all_instances += [i for r in reservations for i in r.instances]
     return all_instances
 
-def get_role_names(region, tags):
+def get_role_names(region, tags, profile):
     filters = []
     all_role_names = []
     for tag in tags:
-        filters.append({ 'tag-key': tag })
+        filters.append({ 'tag-key': tag, 'resource-type': 'instance' })
     for ec2_filter in filters:
-        ec2_conn = ec2.connect_to_region(region)
+        ec2_conn = ec2.connect_to_region(region, profile_name=profile)
         tags = ec2_conn.get_all_tags(filters=ec2_filter)
         all_role_names += [str(t.value) for t in tags]
     return sorted(list(set(all_role_names)))
@@ -40,11 +41,13 @@ else:
     config.set('DEFAULT', 'region', 'ap-southeast-2')
 config.set('DEFAULT', 'role_tag', 'role')
 config.set('DEFAULT', 'host_script', '/usr/local/bin/csshX')
+config.set('DEFAULT', 'profile', 'default')
 config.read("%s/.awstools.conf" % (expanduser("~")))
 
 conf_parser = argparse.ArgumentParser()
 conf_parser.add_argument("-R", "--get-roles", dest="get_roles", default=False, action='store_true', help="Get a list of roles in the region")
 conf_parser.add_argument("-r", "--region", dest="region", default=config.get('DEFAULT', 'region'), help="EC2 region instances are located in")
+conf_parser.add_argument("-p", "--profile", dest="profile", default=config.get('DEFAULT', 'profile'), help="Boto profile name to use")
 conf_parser.add_argument("-t", "--tag", dest="tag", default=config.get('DEFAULT', 'role_tag'), help="The AWS tag name to search for instances with")
 conf_parser.add_argument("-s", "--script_mode", action='store_true', default=False, help="Output in script friendly mode (IP address list only)")
 conf_parser.add_argument("-S", "--script_run_mode", action='store_true', default=False, help="Output in script friendly mode and runs host_script in config. Defaults to csshx")
@@ -67,7 +70,7 @@ filters = []
 all_instances = []
 
 if args.get_roles:
-    role_list = get_role_names(region, tag)
+    role_list = get_role_names(region, tag, args.profile)
     for role in role_list:
         print role
     exit(0)
@@ -78,7 +81,7 @@ else:
         exit(1)
 
 for role in args.role_name:
-    all_instances += get_instances(region, tag, role)
+    all_instances += get_instances(region, tag, role, args.profile)
 
 if not script_mode:
     print "Region: %s" % (region)
@@ -87,7 +90,7 @@ if not script_mode:
 for instance in all_instances:
     if script_mode:
         print instance.private_ip_address
-    else:    
+    else:
         print "%s %s %s %s" % (instance.tags['Name'], instance.id, instance.private_ip_address, instance.ip_address)
 
 if script_run_mode:
